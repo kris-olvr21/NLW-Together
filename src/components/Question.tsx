ba1c10572911dc0f2df75063ce00dfa407e8bd06
Kris <krislane_oliveira21@hotmail.com>
2021-06-28 16:20:23 -0300

    day-5

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,23 +1,34 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
 
 import '../styles/question.scss'
 
 type QuestionProps = {
-    content: string,
+    content: string;
     author: {
-        name: string,
-        avatar: string,
+        name: string;
+        avatar: string;
     };
-    children?: ReactNode,
+    children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
 }
 
 export function Question({
     content,
     author,
     children,
+    isAnswered = false,
+    isHighlighted = false,
 }: QuestionProps) {
     return (
-        <div className="question">
+        <div 
+            className={cx(
+                'question',
+                { answered: isAnswered },
+                { highlighted: isHighlighted && !isAnswered },
+            )}
+        >
             <p>{content}</p>
             <footer>
                 <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,7 +1,9 @@
 import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
-import deleteImg from '../assets/images/delete.svg'
+import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -40,6 +42,18 @@ export function AdminRoom() {
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        });
+    }
+
     return (
         <div id="page-room">
             <header>
@@ -71,7 +85,25 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighlighted={question.isHighlighted}
                             >
+                                {!question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                        >
+                                            <img src={checkImg} alt="Marcar pergunta como respondida"/>
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}
+                                        >
+                                            <img src={answerImg} alt="Dar destaque à pergunta"/>
+                                        </button>
+                                    </>
+                                )}
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}
